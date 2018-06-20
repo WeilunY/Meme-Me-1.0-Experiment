c941c6c2a2563ff11e04b4760437e2c872737d9f
wy-macbook <weilunyao@weiluns-macbook-pro.local>
2018-06-20 15:58:49 +0800

    Initial Commit

---
+++ Meme Me 1.0 ExperimentTests/Meme_Me_1_0_ExperimentTests.swift
@@ -0,0 +1,36 @@
+//
+//  Meme_Me_1_0_ExperimentTests.swift
+//  Meme Me 1.0 ExperimentTests
+//
+//  Created by Weilun Yao on 6/20/18.
+//  Copyright © 2018 Weilun Yao. All rights reserved.
+//
+
+import XCTest
+@testable import Meme_Me_1_0_Experiment
+
+class Meme_Me_1_0_ExperimentTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
